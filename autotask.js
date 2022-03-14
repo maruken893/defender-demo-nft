07ec2b57c19b9a0c6b2e364754f5f5fcae94096d
maruken893 <kentarooooo555@gmail.com>
2022-03-15 00:51:24 +0900

    initial commit

---
+++ autotask.js
@@ -0,0 +1,25 @@
+const ABI = [`function safeMint(address to) public`];
+
+const ADDRESS = ""; // contract address
+const RECIPIENT = ""; //EOA etc.
+
+const { ethers } = require("ethers");
+const {
+  DefenderRelaySigner,
+  DefenderRelayProvider,
+} = require("defender-relay-client/lib/ethers");
+
+//Mints an NFT for a recipient
+async function main(signer, recipient) {
+  const nft = new ethers.Contract(ADDRESS, ABI, signer);
+  const tx = await nft.safeMint(recipient);
+  console.log(`Minted an NFT for ${recipient} in ${tx.hash}`);
+}
+
+// Entrypoint for the Autotask
+exports.handler = async function (params) {
+  const provider = new DefenderRelayProvider(params);
+  const signer = new DefenderRelaySigner(params, provider, { speed: "fast" });
+  console.log(`Using relayer ${await signer.getAddress()}`);
+  await main(signer, RECIPIENT);
+};
